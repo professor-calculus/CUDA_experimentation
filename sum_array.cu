7b9ce9c7c1b399b5235843d9f4238bf9879e5c06
Alexander Titterton <alextitterton@physics.org>
2019-01-03 18:15:33 +0000

    Recursively sums array on GPU, works for arbitrary size within mem limits

--- sum_array.cu
+++ sum_array.cu
@@ -10,7 +10,8 @@
 #define BLOCK_SIZE 1024
 
 //  Size of Array
-#define SOA 1025
+#define SOA 67107840
+//#define SOA 8193
 
 // Allocates an array with random integer entries.
 void randomInit(int* data, int size)
@@ -25,7 +26,7 @@ void randomInit(int* data, int size)
 
 __global__ void ReductionMax2(int *input, int *results, int n)    //take thread divergence into account
 {
-	extern __shared__ int sdata[BLOCK_SIZE];
+	extern __shared__ int sdata[];
 	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned int tx = threadIdx.x;
 	 //load input into __shared__ memory
@@ -59,19 +60,57 @@ __global__ void ReductionMax2(int *input, int *results, int n)    //take thread 
 	}
 }
 
+// Reduce function wrapper
+	void reduce(int* d_a, int* d_b)
+	{
+		int arraySize = SOA;
+		int numBlocks = 1 + ((SOA - 1) / BLOCK_SIZE);
+
+		int* device_intermediate;
+		cudaMalloc(&device_intermediate, sizeof(int)*numBlocks);
+  		cudaMemset(device_intermediate, 0, sizeof(int)*numBlocks);
+
+		int i=1;
+
+		do
+		{
+			std::cout << "GPU Iteration " << i << std::endl;
+			i++;
+
+			//setup execution parameters
+                	dim3 block(BLOCK_SIZE);
+	                dim3 grid(numBlocks);
+
+        	        //execute the kernel
+	                ReductionMax2<<<grid, block, BLOCK_SIZE*sizeof(int)>>>(d_a,device_intermediate,arraySize);
+			arraySize = 1 + ((arraySize - 1) / BLOCK_SIZE);
+
+			// device_in to device_intermediate
+			cudaMemcpy(d_a, device_intermediate, sizeof(int)*numBlocks, cudaMemcpyDeviceToDevice);
+
+			// Update required number of blocks
+			numBlocks = 1 + ((numBlocks - 1) / BLOCK_SIZE);
+
+			cudaFree(device_intermediate);
+			cudaMalloc(&device_intermediate, sizeof(int)*numBlocks);
+		}
+		while(arraySize > BLOCK_SIZE);
+
+		// Now compute the rest
+		ReductionMax2<<<1, BLOCK_SIZE, BLOCK_SIZE*sizeof(int)>>>(d_a,d_b,arraySize);
+	}
+
 
 // get global max element via per-block reductions
 	int main(int argc, char **argv)
 	{
+		// initial num of blocks
 		int num_blocks = 1 + ((SOA - 1) / BLOCK_SIZE);
-		std::cout << num_blocks << " blocks" << std::endl;
+		std::cout << num_blocks << " blocks initially" << std::endl;
 
 		//allocate host memory for array a
 		unsigned int mem_size_a = sizeof(int) * SOA;
 		int* h_a = (int*)malloc(mem_size_a);
-
-		//initialize host memory
-		randomInit(h_a,SOA);
 
 		//allocate device memory
 		int* d_a;
@@ -83,45 +122,18 @@ __global__ void ReductionMax2(int *input, int *results, int n)    //take thread 
 		cudaMemcpy(d_a, h_a, mem_size_a, cudaMemcpyHostToDevice);
 
 		//allocate device memory for temporary results
-		unsigned int mem_size_b = sizeof(int) * num_blocks;
+		unsigned int mem_size_b = sizeof(int) * 1;
 		int* d_b;
 		cudaMalloc((void**) &d_b, mem_size_b);
-		int* h_b = (int*)malloc(mem_size_b);
+		int h_b;
 
-		//allocate device memory for final result
-		unsigned int mem_size_c = sizeof(int) * num_blocks;
-		int* d_c;
-		cudaMalloc((void**) &d_c, mem_size_c);
-
-		//setup execution parameters
-		dim3 block(BLOCK_SIZE);
-		dim3 grid(num_blocks);
-
-		//execute the kernel
-		//first reduce per-block partial maxs
-		ReductionMax2<<<grid, block>>>(d_a,d_b,SOA);
-
-		// Copy partial sums to host
-		cudaMemcpy(h_b, d_b, mem_size_b, cudaMemcpyDeviceToHost);
-
-		//then reduce partial maxs to a final max
-		ReductionMax2<<<grid, block>>>(d_b,d_c,num_blocks);
-
-	       	// allocate host memory for the result
-		int* h_c = (int*)malloc(mem_size_c);
+		// Run our kernel wrapper
+		reduce(d_a, d_b);
 
 		//copy final result from device to host
-		cudaMemcpy(h_c, d_c, mem_size_c, cudaMemcpyDeviceToHost);
+		cudaMemcpy(&h_b, d_b, sizeof(int), cudaMemcpyDeviceToHost);
 
-		int sum_partials = 0;
-		for(int i=0; i<num_blocks; i++)
-		{
-			sum_partials += h_b[i];
-		}
-
-		std::cout << "Sum of partial sums from GPU is: " << sum_partials << std::endl;
-
-                std::cout << "GPU sum: " << h_c[0] << "\n";
+                std::cout << "GPU sum: " << h_b << "\n";
 
 		int tot = 0;
 		for(int i=0; i<SOA; i++)
@@ -132,10 +144,8 @@ __global__ void ReductionMax2(int *input, int *results, int n)    //take thread 
 
 		//clean up memory
 		free(h_a);
-		free(h_c);
 		cudaFree(d_a);
 		cudaFree(d_b);
-		cudaFree(d_c);
 
 		cudaThreadExit();
 
