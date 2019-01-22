09484088656595269b5e393bd3fa88dfed62a0d7
Alexander Titterton <alextitterton@physics.org>
2019-01-22 15:39:48 +0000

    Added program to find max element of array on GPU

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # CUDA_experimentation
 Playing around with CUDA in C/C++, learning the basics and trying to get better!
 
+Compilation: nvcc <script>.cu -o <script>
+Run with ./<script> or nvprof ./<script>
+
 * sum_array.cu: This self-contained script sums the elements of an array recursively. Can handle arrays of any size up to the limits of the grid dimensions of one's card.
+
+* max_array.cu: This script returns the maximum element of an array, iterating in much the same way as sum_array. Loops over blocks on GPU recursively, for arbitrary array size.
--- max_array.cu
+++ max_array.cu
@@ -5,30 +5,31 @@
 #include <math.h>
 #include <time.h>
 #include <ctime>
+#include <algorithm>
 
 // Thread block size
 #define BLOCK_SIZE 1024
 
 //  Size of Array
-//#define SOA 67107840
-//#define SOA 2147483647
+// #define SOA 67107840
+// #define SOA 2147483647
 #define SOA 1147483647
-//#define SOA 8193
+// #define SOA 8193
 
 // Allocates an array with random integer entries.
-void randomInit(unsigned long long int* data, unsigned long long int size)
+void randomInit(int* data, unsigned long long int size)
 {
 	srand( time(0) );
 	for (unsigned long long int i = 0; i < size; ++i)
 	{
-		data[i] = rand() & 10;
+		data[i] = rand() & INT_MAX;
 		//std::cout << data[i] << "\n";
 	}
 }
 
-__global__ void ReductionMax2(unsigned long long int *input, unsigned long long int *results, unsigned long long int n)    //take thread divergence into account
+__global__ void ReductionMax2(int *input, int *results, unsigned long long int n)    //take thread divergence into account
 {
-	extern __shared__ unsigned long long int sdata[];
+	extern __shared__ int sdata[];
 	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned int tx = threadIdx.x;
 	 //load input into __shared__ memory
@@ -38,7 +39,7 @@ __global__ void ReductionMax2(unsigned long long int *input, unsigned long long 
 	}
 	else
 	{
-		sdata[tx] = 0;
+		sdata[tx] = INT_MIN;
 	}
 
 	__syncthreads();
@@ -49,7 +50,8 @@ __global__ void ReductionMax2(unsigned long long int *input, unsigned long long 
 		int index = 2 * offset * tx;
 		if(index < blockDim.x)
 	        {
-			sdata[index] += sdata[index + offset];
+			// sdata[index] += sdata[index + offset];
+			if(sdata[index] < sdata[index + offset]) sdata[index] = sdata[index + offset];
 		}
 		__syncthreads();
 	}
@@ -63,14 +65,14 @@ __global__ void ReductionMax2(unsigned long long int *input, unsigned long long 
 }
 
 // Reduce function wrapper
-	void reduce(unsigned long long int* d_a, unsigned long long int* d_b)
+	void reduce(int* d_a, int* d_b)
 	{
 		unsigned long long int arraySize = SOA;
 		unsigned long long int numBlocks = 1 + ((SOA - 1) / BLOCK_SIZE);
 
-		unsigned long long int* device_intermediate;
-		cudaMalloc(&device_intermediate, sizeof(unsigned long long int)*numBlocks);
-  		cudaMemset(device_intermediate, 0, sizeof(unsigned long long int)*numBlocks);
+		int* device_intermediate;
+		cudaMalloc(&device_intermediate, sizeof(int)*numBlocks);
+  		cudaMemset(device_intermediate, 0, sizeof(int)*numBlocks);
 
 		int i=1;
 
@@ -84,22 +86,22 @@ __global__ void ReductionMax2(unsigned long long int *input, unsigned long long 
 	                dim3 grid(numBlocks);
 
         	        //execute the kernel
-	                ReductionMax2<<<grid, block, BLOCK_SIZE*sizeof(unsigned long long int)>>>(d_a,device_intermediate,arraySize);
+	                ReductionMax2<<<grid, block, BLOCK_SIZE*sizeof(int)>>>(d_a,device_intermediate,arraySize);
 			arraySize = 1 + ((arraySize - 1) / BLOCK_SIZE);
 
 			// device_in to device_intermediate
-			cudaMemcpy(d_a, device_intermediate, sizeof(unsigned long long int)*numBlocks, cudaMemcpyDeviceToDevice);
+			cudaMemcpy(d_a, device_intermediate, sizeof(int)*numBlocks, cudaMemcpyDeviceToDevice);
 
 			// Update required number of blocks
 			numBlocks = 1 + ((numBlocks - 1) / BLOCK_SIZE);
 
 			cudaFree(device_intermediate);
-			cudaMalloc(&device_intermediate, sizeof(unsigned long long int)*numBlocks);
+			cudaMalloc(&device_intermediate, sizeof(int)*numBlocks);
 		}
 		while(arraySize > BLOCK_SIZE);
 
 		// Now compute the rest
-		ReductionMax2<<<1, BLOCK_SIZE, BLOCK_SIZE*sizeof(unsigned long long int)>>>(d_a,d_b,arraySize);
+		ReductionMax2<<<1, BLOCK_SIZE, BLOCK_SIZE*sizeof(int)>>>(d_a,d_b,arraySize);
 	}
 
 
@@ -107,7 +109,7 @@ __global__ void ReductionMax2(unsigned long long int *input, unsigned long long 
 	int main(int argc, char **argv)
 	{
 		// Introduce program
-		std::cout << "Sum the elements of an array on a GPU" << std::endl;
+		std::cout << "Finding max element of an array on a GPU" << std::endl;
 
 		// show memory usage of GPU
         	size_t free_byte ;
@@ -134,7 +136,7 @@ __global__ void ReductionMax2(unsigned long long int *input, unsigned long long 
 		std::cout << num_blocks << " blocks initially" << std::endl;
 
 		//allocate host memory for array a
-		unsigned long long int mem_size_a = sizeof(unsigned long long int) * SOA;
+		unsigned long long int mem_size_a = sizeof(int) * SOA;
 		if(mem_size_a > free_db)
 		{
 			std::cout << "Error: Not enough available GPU memory!" << std::endl;
@@ -142,10 +144,10 @@ __global__ void ReductionMax2(unsigned long long int *input, unsigned long long 
 		}
 
 		std::cout << mem_size_a/1024.0/1024.0 << "MB requested" << std::endl;
-		unsigned long long int* h_a = (unsigned long long int*)malloc(mem_size_a);
+		int* h_a = (int*)malloc(mem_size_a);
 
 		//allocate device memory
-		unsigned long long int* d_a;
+		int* d_a;
 		cudaMalloc((void**) &d_a, mem_size_a);
 
 		randomInit(h_a,SOA);
@@ -154,25 +156,22 @@ __global__ void ReductionMax2(unsigned long long int *input, unsigned long long 
 		cudaMemcpy(d_a, h_a, mem_size_a, cudaMemcpyHostToDevice);
 
 		//allocate device memory for temporary results
-		unsigned long long int mem_size_b = sizeof(long) * 1;
-		unsigned long long int* d_b;
+		int mem_size_b = sizeof(int) * 1;
+		int* d_b;
 		cudaMalloc((void**) &d_b, mem_size_b);
-		unsigned long long int h_b;
+		int h_b;
 
 		// Run our kernel wrapper
 		reduce(d_a, d_b);
 
 		//copy final result from device to host
-		cudaMemcpy(&h_b, d_b, sizeof(long), cudaMemcpyDeviceToHost);
+		cudaMemcpy(&h_b, d_b, sizeof(int), cudaMemcpyDeviceToHost);
 
-                std::cout << "GPU sum: " << h_b << "\n";
+                std::cout << "GPU max: " << h_b << "\n";
 
-		unsigned long long int tot = 0;
-		for(unsigned long long int i=0; i<SOA; i++)
-		{
-			tot += h_a[i];
-		}
-		std::cout << "Old-fashioned way: " << tot << "\n";
+		int max = *std::max_element(h_a, h_a + SOA);
+
+		std::cout << "Old-fashioned way: " << max << "\n";
 
 		//clean up memory
 		free(h_a);
